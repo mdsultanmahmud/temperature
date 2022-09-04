39ee4fa89099825cdad5f09c057a80a84fa78f3d
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-09-04 18:30:30 +0600

    load data from weather api

--- apps.js
+++ apps.js
@@ -1,1 +1,11 @@
-console.log('hi, this is weither app.js')
+//do not show your api key in this way
+const apiKey = 'f3200c9f648616521f59ccc844d74042'
+
+const loadTemperatureData = (city) =>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+loadTemperatureData('rajshahi')
